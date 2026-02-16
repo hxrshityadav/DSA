0ffcd8d6280875fe0cf60ba9b5142b84e1c909e7
Harshit Yadav <85622854+hxrshityadav@users.noreply.github.com>
2026-02-16 14:56:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
@@ -1,52 +1,41 @@
-import java.util.ArrayList;
-
 class Solution {
     public static ArrayList<Integer> findUnion(int a[], int b[]) {
-        ArrayList<Integer> union = new ArrayList<>();
-        int i = 0, j = 0;
-        int n = a.length;
-        int m = b.length;
-
+        // code here
+        ArrayList<Integer> result = new ArrayList<>();
         
-        while (i < n && j < m) {
-            
-            if (a[i] < b[j]) {
-                addIfNew(union, a[i]);
-                i++;
+        int i = 0, j= 0;
+        
+        while (i <a.length && j<b.length){
+            int val;
+            if(a[i]< b[j]){
+                val=a[i++];
             }
-            
-            else if (b[j] < a[i]) {
-                addIfNew(union, b[j]);
-                j++;
-            }
-            
-            else {
-                addIfNew(union, a[i]);
+            else if(a[i]> b[j]){
+                val=b[j++];
+            } 
+            else{
+                val= a[i];
                 i++;
                 j++;
+            }
+            
+            if(result.size() == 0 || result.get(result.size() -1)!= val){
+                result.add(val);
             }
         }
-
         
-        while (i < n) {
-            addIfNew(union, a[i]);
+        while(i< a.length){
+            if(result.size()== 0 || result.get(result.size()-1)!= a[i]){
+                result.add(a[i]);
+            }
             i++;
         }
-
-        
-        while (j < m) {
-            addIfNew(union, b[j]);
+        while(j<b.length){
+            if(result.size()==0 || result.get(result.size()-1)!=b[j]){
+                result.add(b[j]);
+            }
             j++;
         }
-
-        return union;
-    }
-
-    
-    private static void addIfNew(ArrayList<Integer> list, int val) {
-        
-        if (list.isEmpty() || list.get(list.size() - 1) != val) {
-            list.add(val);
-        }
+        return result;
     }
 }

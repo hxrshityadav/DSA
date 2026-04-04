653e4f09dde35b2e0820e45ec79ae0f025bfb845
Harshit Yadav <85622854+hxrshityadav@users.noreply.github.com>
2026-04-04 14:31:01 +0530

    Delete Difficulty: Basic directory

--- Difficulty: Basic/Factorial/factorial.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    // Function to calculate factorial of a number.
-    int factorial(int n) {
-        // code here
-        if ( n == 0 || n == 1) return 1;
-        return n * factorial(n - 1);
-    }
-}
--- Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public static int findSum(int n) {
-        // code here
-        if(n == 0) return 0;
-        return n + findSum(n - 1);
-    }
-}
